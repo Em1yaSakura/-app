265021b04aabb042f8facbf062238081af88d963
kacy2233 <mi994135@163.com>
2026-05-11 09:58:57 +0800

    v1.3 新模式按钮 + 进度显示 + 首次答对新词跳强度40

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.englishapp"
         minSdk = 33
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
